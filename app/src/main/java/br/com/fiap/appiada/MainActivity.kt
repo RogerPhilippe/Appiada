1ab8439bda649bd0cff37bcf67e78095f3bddf75
Logon Aluno <logonaluno@local.com>
2020-01-22 22:53:36 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/appiada/MainActivity.kt
@@ -0,0 +1,29 @@
+package br.com.fiap.appiada
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        showJoker()
+
+        btTellJoker.setOnClickListener {
+            showJoker()
+        }
+
+    }
+
+    private fun showJoker() {
+
+        val jokers = resources.getStringArray(R.array.jokers)
+        val index = Random().nextInt(jokers.size)
+        tvJoker.text = jokers[index]
+
+    }
+}
